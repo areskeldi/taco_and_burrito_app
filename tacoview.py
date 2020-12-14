f1748ebc0d63d68442723b469671bac57b140d7d
areskeldi <emilbekuuluy@berea.edu>
2020-12-13 21:08:34 -0500

    fetched the sql data from tacos db

---
+++ tacoview.py
@@ -0,0 +1,39 @@
+from kivy.garden.mapview import MapView
+from kivy.clock import Clock
+from kivy.app import App
+from tacomarker import TacoMarker
+
+
+class TacoView(MapView):
+    tacos_timer = None
+    taco_names=[]
+
+    def start_getting_tacos_in_fov(self):
+        # After one second, get the markets in the field of view
+        try:
+            self.tacos_timer.cancel()
+        except:
+            pass
+
+        self.tacos_timer = Clock.schedule_once(self.get_tacos_in_fov, 1)
+
+    def get_tacos_in_fov(self, *args):
+        # Get reference to main app and the database cursor
+        min_lat, min_lon, max_lat, max_lon = self.get_bbox()
+        app = App.get_running_app()
+        sql_statement = "SELECT DISTINCT name, longitude, latitude FROM tacos_and_burritos WHERE longitude > %s AND longitude < %s AND latitude > %s AND latitude < %s "%(min_lon, max_lon, min_lat, max_lat)
+        app.cursor.execute(sql_statement)
+        tacos = app.cursor.fetchall()
+        for taco in tacos:
+            name=taco[1]
+            if name in self.taco_names:
+                continue
+            else:
+                self.add_taco(taco)
+
+    def add_taco(self, taco):
+        lon, lat = taco[1], taco[2]
+        marker = TacoMarker(lon=lon, lat=lat, source='burrito (2).png')
+        self.add_widget(marker)
+        name = taco[0]
+        self.taco_names.append(name)
