fedd26594373b32e68dfd239754052039c19226a
areskeldi <emilbekuuluy@berea.edu>
2020-12-15 19:54:21 -0500

    added TacoMarker class

--- tacoview.py
+++ tacoview.py
@@ -21,7 +21,7 @@ class TacoView(MapView):
         # Get reference to main app and the database cursor
         min_lat, min_lon, max_lat, max_lon = self.get_bbox()
         app = App.get_running_app()
-        sql_statement = "SELECT DISTINCT name, longitude, latitude FROM tacos_and_burritos WHERE longitude > %s AND longitude < %s AND latitude > %s AND latitude < %s "%(min_lon, max_lon, min_lat, max_lat)
+        sql_statement = "SELECT * FROM tacos_and_burritos WHERE longitude > %s AND longitude < %s AND latitude > %s AND latitude < %s "%(min_lon, max_lon, min_lat, max_lat)
         app.cursor.execute(sql_statement)
         tacos = app.cursor.fetchall()
         for taco in tacos:
@@ -32,8 +32,9 @@ class TacoView(MapView):
                 self.add_taco(taco)
 
     def add_taco(self, taco):
-        lon, lat = taco[1], taco[2]
+        lon, lat = taco[7], taco[6]
         marker = TacoMarker(lon=lon, lat=lat, source='burrito (2).png')
+        marker.taco_data=taco
         self.add_widget(marker)
-        name = taco[0]
+        name = taco[16]
         self.taco_names.append(name)
