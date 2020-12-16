d92f5f7f0665c993be89a8244ed79949e4f31c74
areskeldi <emilbekuuluy@berea.edu>
2020-12-15 19:52:16 -0500

    reduced menu size

--- tacomarker.py
+++ tacomarker.py
@@ -1,10 +1,11 @@
 from kivy.garden.mapview import MapMarkerPopup
+from tacomenu import TacoMenu
 
 class TacoMarker(MapMarkerPopup):
     taco_data = []
 
     def on_release(self):
         # Open up the LocationPopupMenu
-        menu = LocationPopupMenu(self.taco_data)
-        menu.size_hint = [.8, .9]
+        menu = TacoMenu(self.taco_data)
+        menu.size_hint=[.8,.9 ]
         menu.open()
